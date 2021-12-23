9a8b5c7432cfd860f7cdca2cd49dbbafc741ec32
WencesJ <wences2512@gmail.com>
2021-12-23 02:09:03 +0100

    second commit

--- src/api/talks/talk.spec.ts
+++ src/api/talks/talk.spec.ts
@@ -8,6 +8,6 @@
 //     });
 // });
 
-// Due to the limited time which i have left to submit i'm not able to write all test cases as i spent most of the time optimizing the structure and testing with POSTMAN, but I have thoroughly tested all endpoints with POSTMAN, and all endpoints are working perfectly.
+// // Due to the limited time which i have left to submit i'm not able to write all test cases as i spent most of the time optimizing the structure and testing with POSTMAN, but I have thoroughly tested all endpoints with POSTMAN, and all endpoints are working perfectly.
 
-//Thank you.
+// //Thank you.
